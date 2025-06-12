61b74eca23b209067ad2a147f797e67ac5b53c84
Jefferson Lima <jbldesenvolvimento@gmail.com>
2025-06-12 13:58:18 -0300

    Importando middlewares

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,31 @@
+require("dotenv").config();
+
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
 
-const port = 5000;
+const port = process.env.port;
 
 const app = express();
 
 // config JSON ad form data response
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// Solve CORS
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+
+// routes
+const router = require("./routes/Router.js");
+
+// __dirname - Nome do Diretorio Atual
+// Upload directory
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+// DB Connection
+require("./config/db.js");
+
+app.use(router);
 
 app.listen(port, () => {
   console.log(`App rodando na porta ${port}`);
