79b1c71457c01213fd8aaf982d87a80d5686a6af
Jefferson Lima <jbldesenvolvimento@gmail.com>
2025-06-23 16:00:47 -0300

    Criação classe authService.js onde faz as requisições de POST para o BackEnd

---
+++ frontend/src/services/authService.js
@@ -0,0 +1,24 @@
+import { API, requestConfig } from "../utils/config";
+
+// Register an user
+const register = async (user) => {
+  const config = requestConfig("POST", user);
+
+  try {
+    const res = await fetch(`${API}/users/register`, config)
+      .then((res) => res.json())
+      .catch((err) => err);
+
+    if (res) {
+      localStorage.setItem("user", JSON.stringify(res));
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const authService = {
+  register,
+};
+
+export default authService;
