1b24d4198ba1f6a9285500c8141bcbe1937d116d
Jefferson Lima <jbldesenvolvimento@gmail.com>
2025-08-07 12:03:06 -0300

    Autenticação do EditProfile

--- frontend/src/pages/EditProfile/EditProfile.js
+++ frontend/src/pages/EditProfile/EditProfile.js
@@ -1,37 +1,103 @@
 import "./EditProfile.css";
 
-const EditProfile = () => {
+import { uploads } from "../../utils/config";
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        // Aqui você pode adicionar a lógica para enviar os dados do formulário
-        console.log("Formulário enviado");
+// Hooks
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+// Redux
+import { profile, resetMessage } from "../../slices/userSlice";
+
+// Componentes
+import Message from "../../components/Message";
+
+const EditProfile = () => {
+  const dispatch = useDispatch();
+  //const { user: authUser } = useSelector((state) => state.auth);
+  const { user, loading, error, message } = useSelector((state) => state.user);
+
+  // States
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [profileImage, setProfileImage] = useState("");
+  const [bio, setBio] = useState("");
+  const [previewImage, setPreviewImage] = useState("");
+
+  // Load user data
+  useEffect(() => {
+    dispatch(profile());
+  }, [dispatch]);
+
+  // Fill form with user data
+  useEffect(() => {
+    if (user) {
+      setName(user.name);
+      setEmail(user.email);
+      setBio(user.bio);
     }
+  }, [user]);
+
+  console.log("User data:", user);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Aqui você pode adicionar a lógica para enviar os dados do formulário
+    console.log("Formulário enviado");
+  };
 
   return (
     <div id="edit-profile">
       <h2>Editar Perfil</h2>
-        <p className="subtitle">Adicione uma imagem de perfil e conte mais sobre você...</p>
-        {/* Formulário de edição de perfil */}   
-        <form onSubmit={handleSubmit}> 
-          <input type="text" placeholder="Nome" />
-          <input type="email" placeholder="Email" />
-          <label>
-            <span>Imagem de perfil:</span>
-            <input type="file" />
-          </label>
-          <label>
-            <span>Sobre você:</span>
-            <input type="text" placeholder="Descrição do perfil" />
-          </label>
-          <label>
-            <span>Senha para alterar:</span>
-            <input type="password" placeholder="Digite sua nova senha" />    
-          </label>
-          <input type="submit" value="Atualizar" />
-        </form>
+      <p className="subtitle">
+        Adicione uma imagem de perfil e conte mais sobre você...
+      </p>
+      {/* Formulário de edição de perfil */}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Nome"
+          value={name || ""}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          value={email || ""}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label>
+          <span>Imagem de perfil:</span>
+          <input
+            type="file"
+            onChange={(e) => setProfileImage(e.target.files[0])}
+          />
+        </label>
+        <label>
+          <span>Sobre você:</span>
+          <input
+            type="text"
+            placeholder="Descrição do perfil"
+            value={bio || ""}
+            onChange={(e) => setBio(e.target.value)}
+          />
+        </label>
+        <label>
+          <span>Senha para alterar:</span>
+          <input
+            type="password"
+            placeholder="Digite sua nova senha"
+            value={password || ""}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        {!loading && <input type="submit" value="Atualizar" />}
+        {loading && <input type="submit" value="Aguarde..." disabled />}
+        {error && <Message msg={error} type="error" />}
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default EditProfile
+export default EditProfile;
--- frontend/src/slices/userSlice.js
+++ frontend/src/slices/userSlice.js
@@ -2,53 +2,50 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import userService from "../services/userService";
 
 const initialState = {
-    user: null,
-    error: false,
-    loading: false,
-    success: false,
-    message: null
+  user: null,
+  error: false,
+  loading: false,
+  success: false,
+  message: null,
 };
 
 // Get user details
 export const profile = createAsyncThunk(
-    "user/profile",
-    async (user, thunkAPI) => {
-        
-        const token = thunkAPI.getState().auth.token;
-        const response = await userService.profile(user, token);    
-        if (response.error) {
-            return thunkAPI.rejectWithValue(response.error);
-        }
-        return response;
+  "user/profile",
+  async (user, thunkAPI) => {
+    const token = thunkAPI.getState().auth.user.token;
+    const response = await userService.profile(user, token);
+    if (response.error) {
+      return thunkAPI.rejectWithValue(response.error);
     }
+    return response;
+  }
 );
 
-         
-
 export const userSlice = createSlice({
-    name: "user",
-    initialState,
-    reducers: {
-        reset: (state) => {
-            state.error = false;
-            state.loading = false;
-            state.success = false;
-            state.message = null;
-        },
+  name: "user",
+  initialState,
+  reducers: {
+    reset: (state) => {
+      state.error = false;
+      state.loading = false;
+      state.success = false;
+      state.message = null;
     },
-    extraReducers: (builder) => {
-        builder
-                    .addCase(profile.pending, (state) => {
-                        state.loading = true;
-                        state.error = false;
-                    })
-                    .addCase(profile.fulfilled, (state, action) => {
-                        state.loading = false;
-                        state.success = true;
-                        state.error = null;
-                        state.user = action.payload;                        
-                    })                    
-                }
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(profile.pending, (state) => {
+        state.loading = true;
+        state.error = false;
+      })
+      .addCase(profile.fulfilled, (state, action) => {
+        state.loading = false;
+        state.success = true;
+        state.error = null;
+        state.user = action.payload;
+      });
+  },
 });
 
 export const { resetMessage } = userSlice.actions;
