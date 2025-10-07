ad27a73b84bdcfaa9c9eddb6d28ecfd262c5f522
Jefferson Lima <jbldesenvolvimento@gmail.com>
2025-10-07 13:23:02 -0300

    Ajuste na conexão com o banco de dados - MongoDB

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,14 +7,16 @@ const dbPassword = process.env.DB_PASS;
 const conn = async () => {
   try {
     const dbConn = await mongoose.connect(
+    //  `mongodb+srv://jeffersonlimapb:<db_password>@cluster0.plh1ecc.mongodb.net/?retryWrites=true&w=majority`, 
       `mongodb+srv://${dbUser}:${dbPassword}@cluster0.plh1ecc.mongodb.net/?retryWrites=true&w=majority`,
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
       }
-    );
+    ).then(() => console.log("Conectado ao MongoDB"))
+    .catch(err => console.error(err));
 
-    console.log("Conectou ao banco!");
+   // console.log("Conectou ao banco!");
 
     return dbConn;
   } catch (error) {
